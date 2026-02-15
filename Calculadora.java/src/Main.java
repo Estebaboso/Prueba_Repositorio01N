bc6087dd7ce84af0f7cf8c332e4588498f2cdd1c
Estebaboso <juane.castanog@uqvirtual.edu.co>
2026-02-15 13:27:11 -0500

    Calculadora 0.1

---
+++ Calculadora.java/src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+    public class Main {
+
+        static Scanner scanner = new Scanner(System.in);
+
+        public void main(String[] args) {
+
+            int valorI = ingresarEntero("Ingrese el primer valor que desea operar: ");
+            int valorII = ingresarEntero("Ingrese el segundo valor que desea operar: ");
+            char opcion = ingresarCaracter("Ingrese la operación (+, -, *, /): ");
+            int operacion = calcularOperacion(valorI, valorII, opcion);
+            String mensaje = generarMensaje(valorI, valorII, opcion, operacion);
+            System.out.println(mensaje);
+        }
+
+        public int ingresarEntero(String entrada) {
+            System.out.print(entrada);
+            return scanner.nextInt();
+        }
+
+        public char ingresarCaracter(String entrada) {
+            System.out.print(entrada);
+            return scanner.next().charAt(0);
+        }
